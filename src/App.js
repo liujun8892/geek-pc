519a3d4d9b4a779fda93131b584db2c17b009341
liujun <ajun_8892@163.com>
2023-07-17 19:53:49 +0800

    项目搭建配置路由

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import Home from './pages/layout';
+import Loging from './pages/login';
 function App() {
-  return <div className="App">我是根组件</div>;
+  return (
+    <Router>
+      <div className="app">
+        <Link to="/login">登录</Link>
+        <Link to="/home">首页</Link>
+
+        <Routes>
+          <Route path="/home" Component={Home}></Route>
+          <Route path="/login" Component={Loging}></Route>
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;

f91c002c0d0e32eaf0536ee87918fe5540410c1a
liujun <ajun_8892@163.com>
2023-07-17 21:01:00 +0800

    complete login base framework

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import { DatePicker } from 'antd';
+
 import Home from './pages/layout';
-import Loging from './pages/login';
+import Login from './pages/login';
 function App() {
   return (
     <Router>
       <div className="app">
-        <Link to="/login">登录</Link>
-        <Link to="/home">首页</Link>
-
         <Routes>
           <Route path="/home" Component={Home}></Route>
-          <Route path="/login" Component={Loging}></Route>
+          <Route path="/login" Component={Login}></Route>
         </Routes>
       </div>
     </Router>
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
+import { Card } from 'antd';
+import './index.css';
+import Logo from '../../assets/logo.png';
 
 export default class index extends Component {
   render() {
-    return <div>登录页面</div>;
+    return (
+      <div className="login">
+        <Card className="login-card">
+          <img src={Logo} className="login-logo" alt="极客园" />
+        </Card>
+      </div>
+    );
   }
 }
